51970919629423ae6330c341ce1a9bd0e57fe782
Remy <shingiroremy303@gmail.com>
2025-05-30 11:21:12 +0200

    added comments

--- src/useEffect.jsx
+++ src/useEffect.jsx
@@ -2,15 +2,15 @@
 import { useState, useEffect } from "react";
 
 function Timer(){
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0); //this is a useState hook to handle the states in our timer.
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            setCount((count) => count + 1);
-        },1000)
+    useEffect(()=>{    // a useEffect hook to handle effects like timer.
+        setTimeout(()=>{  //setting a timeout to count.
+            setCount((count) => count + 1); // our setter function to increment our timer.
+        },1000) // the time delay of one second.
     });
     return(
-        <h1>I have rendered {count} times!!</h1>
+        <h1>I have rendered {count} times!!</h1> //returning our initial and updated state.
     );
 
 };
